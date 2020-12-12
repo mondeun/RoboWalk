a4c8e2431b9db571fa2495198e637127c32b367a
Kenneth Glawe <kenneth.glawe@gmail.com>
2020-12-12 15:58:12 +0100

    implement main method

--- RoboWalk/Program.cs
+++ RoboWalk/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using RoboWalk.Interfaces;
 
 namespace RoboWalk
 {
@@ -6,7 +8,35 @@ namespace RoboWalk
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (!int.TryParse(Console.ReadLine(), out var width))
+                return;
+            if (!int.TryParse(Console.ReadLine(), out var height))
+                return;
+
+            if (!int.TryParse(Console.ReadLine(), out var x))
+                return;
+            if (!int.TryParse(Console.ReadLine(), out var y))
+                return;
+            var directionString = Console.ReadLine();
+            
+            IRoom room = new Room(width, height);
+            IRobot robot = new Robot(room);
+
+            if (robot.SetPosition(x, y, GetDirection(directionString)))
+            {
+                var newPosition = robot.ProcessCommands(Console.ReadLine());
+                Console.WriteLine(newPosition);
+            }
         }
+
+        static Direction GetDirection(string input) =>
+            input.ToUpper()[0].ToString() switch
+            {
+                "N" => Direction.N,
+                "E" => Direction.E,
+                "S" => Direction.S,
+                "W" => Direction.W,
+                _ => Direction.N
+            };
     }
 }
